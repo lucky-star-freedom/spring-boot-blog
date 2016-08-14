95e7db2d1d1407fbd5c8b8a144080847b07e8a31
arcelan.sara <lu_zhiyuan@yeah.net>
2016-08-14 16:42:15 +0800

    add create and destroy API

--- src/main/resources/static/js/views/blog-view.js
+++ src/main/resources/static/js/views/blog-view.js
@@ -1,42 +1,49 @@
 /*global define*/
 define([
-	'jquery',
-	'underscore',
-	'backbone',
-	'text!templates/blogs.html',
-	'common'
+    'jquery',
+    'underscore',
+    'backbone',
+    'text!templates/blogs.html',
+    'common'
 ], function ($, _, Backbone, blogsTemplate, Common) {
-	'use strict';
+    'use strict';
 
-	var BlogView = Backbone.View.extend({
+    var BlogView = Backbone.View.extend({
 
-		tagName:  'li',
+        tagName: 'li',
 
-		template: _.template(blogsTemplate),
+        template: _.template(blogsTemplate),
 
-		// The DOM events specific to an item.
-		events: {
-			'click .view':	'consoleOutput'
-		},
+        // The DOM events specific to an item.
+        events: {
+            'click .destroy': 'clear',
+            'click .view': 'consoleOutput'
+        },
 
-		// The BlogView listens for changes to its model, re-rendering. Since there's
-		// a one-to-one correspondence between a **Todo** and a **BlogView** in this
-		// app, we set a direct reference on the model for convenience.
-		initialize: function () {
-			this.listenTo(this.model, 'change', this.render);
-		},
+        // The BlogView listens for changes to its model, re-rendering. Since there's
+        // a one-to-one correspondence between a **Blog** and a **BlogView** in this
+        // app, we set a direct reference on the model for convenience.
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
 
-		// Re-render the titles of the todo item.
-		render: function () {
-			this.$el.html(this.template(this.model.toJSON()));
+        // Re-render the titles of the todo item.
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
 
-			return this;
-		},
+            return this;
+        },
 
-		consoleOutput: function () {
-			console.log("you are click view");
-		}
-	});
+        // Remove the item, destroy the model from *back end* and delete its view.
+        clear: function () {
+            this.model.destroy();
+        },
 
-	return BlogView;
+        consoleOutput: function () {
+            console.log("you are click view");
+        }
+    });
+
+    return BlogView;
 });
