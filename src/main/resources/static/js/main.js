8ef550b7da50dd8192ba8ddc001df21460be17f5
arcelan <lu_zhiyuan@yeah.net>
2016-08-15 15:42:20 +0800

    Reorganize js lib

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,31 +3,31 @@
 
 // Require.js allows us to configure shortcut alias
 require.config({
-	// The shim config allows us to configure dependencies for
-	// scripts that do not call define() to register a module
-	shim: {
-		underscore: {
-			exports: '_'
-		},
-		backbone: {
-			deps: [
-				'underscore',
-				'jquery'
-			],
-			exports: 'Backbone'
-		}
-	},
-	paths: {
-		jquery: 'lib/jquery',
-		underscore: 'lib/underscore',
-		backbone: 'lib/backbone',
-		text: 'lib/text'
-	}
+    // The shim config allows us to configure dependencies for
+    // scripts that do not call define() to register a module
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: [
+                'underscore',
+                'jquery'
+            ],
+            exports: 'Backbone'
+        }
+    },
+    paths: {
+        jquery: '../vendor/jquery/jquery',
+        underscore: '../vendor/underscore/underscore',
+        backbone: '../vendor/backbone/backbone',
+        text: '../vendor/requirejs-text/text'
+    }
 });
 
 require([
-	'views/app-view'
+    'views/app-view'
 ], function (AppView) {
-	// Initialize the application view
-	new AppView();
+    // Initialize the application view
+    new AppView();
 });
