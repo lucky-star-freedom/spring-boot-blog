53611d90fdaa94ead494f3260b525926226aba96
arcelan <lu_zhiyuan@yeah.net>
2016-08-19 17:46:50 +0800

    delete dummy sql

--- sql/insert_dummy.sql
+++
@@ -1,5 +0,0 @@
-USE spring_boot_blog;
-
-INSERT INTO blog(title, content, created_at, likes) VALUE ('Python', 'Python is cool', now(), 0);
-INSERT INTO blog(title, content, created_at, likes) VALUE ('Java', 'Java is cool', now(), 0);
-INSERT INTO blog(title, content, created_at, likes) VALUE ('C', 'C is cool', now(), 0);
