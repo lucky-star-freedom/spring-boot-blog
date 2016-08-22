973e594e89ef6242c65e89cd1af14154deeab5b4
cornerkick <littlefreepointer@gmail.com>
2016-08-22 17:07:14 +0800

    Add blog detail template.

--- src/main/java/com/cornerkick/controller/BlogController.java
+++ src/main/java/com/cornerkick/controller/BlogController.java
@@ -1,16 +1,26 @@
 package com.cornerkick.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by cornerkick on 16/8/11.
  */
 @Controller
+@RequestMapping("blogs")
 public class BlogController {
 
-    @RequestMapping("/blogs")
+    @RequestMapping("")
     public String index() {
         return "blog/index";
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public String detail(@PathVariable Long id, Model model) {
+        model.addAttribute("blogId", id);
+        return "blog/detail";
     }
 }
